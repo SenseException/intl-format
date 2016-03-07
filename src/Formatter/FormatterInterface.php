fda4cacb3f4886497e8889ca24bd07c08cde65ec
Claudio Zizza <claudio@budgegeria.de>
2016-03-07 23:47:00 +0100

    Add Formatter interface

---
+++ src/Formatter/FormatterInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Budgegeria\IntlFormat\Formatter;
+
+interface FormatterInterface
+{
+    /**
+     * Formats the given value.
+     *
+     * Formats the given value by a formatting strategy that is
+     * mapped by the type specifier.
+     *
+     * @param string $typeSpecifier
+     * @param mixed $value
+     * @return string
+     */
+    public function formatValue($typeSpecifier, $value);
+
+    /**
+     * Check if Format has the given type specifier.
+     *
+     * Checks if this Formatter class has the given type specifier to
+     * handle the formatting. If it returns true, the Formatter class
+     * is able to handle the formatting using FormatterInterface::formatValue().
+     *
+     * @param string $typeSpecifier The type specifier without the prefix char.
+     * @return bool
+     */
+    public function has($typeSpecifier);
+}
