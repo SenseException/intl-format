4ea04f5e15ebd2d09d13c7a57b5566cb3a50caad
Claudio Zizza <claudio@budgegeria.de>
2016-05-29 21:28:00 +0200

    Create message parser class structure

---
+++ src/MessageParser/MessageMetaData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Budgegeria\IntlFormat\MessageParser;
+
+/**
+ * The result of a parsed message.
+ *
+ * @internal
+ */
+class MessageMetaData
+{
+    /**
+     * The parsed message.
+     *
+     * Each element in this array contains either plain text or the type specifier in its pure specified format defined
+     * by the MessageParser. Each element has an unique integer key that is referenced by $typeSpecifiers.
+     *
+     * @var array
+     */
+    public $parsedMessage = [];
+
+    /**
+     * The list of type specifiers of the parsed message.
+     *
+     * This array only contains normalized type specifiers that are used in the Formatter classes. The key is an integer
+     * value, that has the same value as its position in the $parsedMessage array.
+     *
+     * @var array
+     */
+    public $typeSpecifiers = [];
+
+    /**
+     * The list of values.
+     *
+     * This list contains the values, that need to be formatted by the Formatter classes. The values are already swapped
+     * and in the same order as the $typeSpecifiers representing it.
+     *
+     * @var array
+     */
+    public $values = [];
+}
