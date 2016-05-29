5de9f3ee0f3f2422d3e0606ac9c4c6df6e39e19f
Claudio Zizza <claudio@budgegeria.de>
2016-05-29 22:14:33 +0200

    Create new unit test class for IntlFormat

---
+++ tests/IntlFormatTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Budgegeria\IntlFormat\Tests;
+
+use Budgegeria\IntlFormat\Formatter\FormatterInterface;
+use Budgegeria\IntlFormat\IntlFormat;
+use Budgegeria\IntlFormat\MessageParser\MessageMetaData;
+use Budgegeria\IntlFormat\MessageParser\MessageParserInterface;
+
+class IntlFormatTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Basic format test
+     */
+    public function testFormat()
+    {
+        $message = 'Hello "{{world}}", how are you';
+
+        $formatter = $this->getMock(FormatterInterface::class);
+        $formatter->expects($this->once())
+            ->method('has')
+            ->with('world')
+            ->willReturn(true);
+        $formatter->expects($this->once())
+            ->method('formatValue')
+            ->with('world', 'island')
+            ->willReturn('island');
+
+        $parsed = new MessageMetaData();
+        $parsed->typeSpecifiers = [1 => 'world'];
+        $parsed->values = ['island'];
+        $parsed->parsedMessage = ['Hello "', '{{world}}', '", how are you'];
+
+        $parser = $this->getMock(MessageParserInterface::class);
+        $parser->expects($this->once())
+            ->method('parseMessage')
+            ->with($message, ['island'])
+            ->willReturn($parsed);
+
+        $intlFormat = new IntlFormat([$formatter], $parser);
+
+        $this->assertSame('Hello "island", how are you', $intlFormat->format($message, 'island'));
+    }
+
+    /**
+     * %date is an unknown type specifier in this test.
+     */
+    public function testMissingTypeSpecifier()
+    {
+        $message = 'Hello {{world}}, Today is {{date}}';
+
+        $formatter = $this->getMock(FormatterInterface::class);
+        $formatter->expects($this->atLeastOnce())
+            ->method('has')
+            ->willReturnMap([
+                ['world', true],
+                ['date', false]
+            ]);
+        $formatter->expects($this->once())
+            ->method('formatValue')
+            ->with('world', 'island')
+            ->willReturn('island');
+
+        $dateTime = new \DateTime();
+
+        $parsed = new MessageMetaData();
+        $parsed->typeSpecifiers = [1 => 'world', 3 => 'date'];
+        $parsed->values = ['island', $dateTime];
+        $parsed->parsedMessage = ['Hello ', '{{world}}', ', Today is ', '{{date}}'];
+
+        $parser = $this->getMock(MessageParserInterface::class);
+        $parser->expects($this->once())
+            ->method('parseMessage')
+            ->with($message, ['island', $dateTime])
+            ->willReturn($parsed);
+
+        $intlFormat = new IntlFormat([$formatter], $parser);
+
+        $this->assertSame('Hello island, Today is {{date}}', $intlFormat->format($message, 'island', $dateTime));
+    }
+
+    /**
+     * More type specifier than values.
+     *
+     * @expectedException \Budgegeria\IntlFormat\Exception\InvalidTypeSpecifierException
+     */
+    public function testInvalidValueTypeSpecifierCount()
+    {
+        $message = 'Hello {{world}}, Today is {{date}}';
+
+        $parsed = new MessageMetaData();
+        $parsed->typeSpecifiers = [1 => 'world', 3 => 'date'];
+        $parsed->values = ['island'];
+        $parsed->parsedMessage = ['Hello ', '{{world}}', ', Today is ', '{{date}}'];
+
+        $parser = $this->getMock(MessageParserInterface::class);
+        $parser->expects($this->once())
+            ->method('parseMessage')
+            ->with($message, ['island'])
+            ->willReturn($parsed);
+
+        $intlFormat = new IntlFormat([], $parser);
+        $intlFormat->format($message, 'island');
+    }
+
+    /**
+     * Less type specifier than values.
+     *
+     * @expectedException \Budgegeria\IntlFormat\Exception\InvalidTypeSpecifierException
+     */
+    public function testEscapedInvalidTypeSpecifierCount()
+    {
+        $message = 'Hello %%world';
+
+        $parsed = new MessageMetaData();
+        $parsed->typeSpecifiers = [];
+        $parsed->values = ['island'];
+        $parsed->parsedMessage = ['Hello ', '%world'];
+
+        $parser = $this->getMock(MessageParserInterface::class);
+        $parser->expects($this->once())
+            ->method('parseMessage')
+            ->with($message, ['island'])
+            ->willReturn($parsed);
+
+        $intlFormat = new IntlFormat([], $parser);
+        $intlFormat->format($message, 'island');
+    }
+}
