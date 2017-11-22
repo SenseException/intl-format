7ed8bdfcea4f11f3afc7f19b8b8b9e24b50ab282
Claudio Zizza <claudio@budgegeria.de>
2017-11-22 23:45:01 +0100

    Create PrecisionNumberFormatter

---
+++ src/Formatter/PrecisionNumberFormatter.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+namespace Budgegeria\IntlFormat\Formatter;
+
+use Budgegeria\IntlFormat\Exception\InvalidValueException;
+use MessageFormatter as Message;
+
+class PrecisionNumberFormatter implements FormatterInterface
+{
+    /**
+     * @var string
+     */
+    private static $matchPattern = '/^\.([0-9]+)number$/';
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @param string $locale
+     */
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function formatValue(string $typeSpecifier, $value) : string
+    {
+        if (!is_numeric($value)) {
+            throw InvalidValueException::invalidValueType($value, ['integer', 'double']);
+        }
+
+        preg_match(self::$matchPattern, $typeSpecifier, $matches);
+
+        $fractionalDigits = str_repeat('0', (int) $matches[1]);
+
+        return Message::formatMessage($this->locale, sprintf('{0,number,#,##0.%s}', $fractionalDigits), [$value]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has(string $typeSpecifier) : bool
+    {
+        return 1 === preg_match(self::$matchPattern, $typeSpecifier);
+    }
+}
---
+++ tests/Formatter/PrecisionNumberFormatterTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Budgegeria\IntlFormat\Tests\Formatter;
+
+use Budgegeria\IntlFormat\Exception\InvalidValueException;
+use Budgegeria\IntlFormat\Formatter\PrecisionNumberFormatter;
+use PHPUnit\Framework\TestCase;
+
+class PrecisionNumberFormatterTest extends TestCase
+{
+    /**
+     * @dataProvider provideTypeSpecifier
+     *
+     * @param string $typeSpecifier
+     */
+    public function testHas(string $typeSpecifier)
+    {
+        $messageFormatter = new PrecisionNumberFormatter('de_DE');
+
+        self::assertTrue($messageFormatter->has($typeSpecifier));
+    }
+
+    public function testHasIsFalse()
+    {
+        $messageFormatter = new PrecisionNumberFormatter('de_DE');
+
+        self::assertFalse($messageFormatter->has('.number'));
+        self::assertFalse($messageFormatter->has('3number'));
+        self::assertFalse($messageFormatter->has('.3numbr'));
+    }
+
+    public function testFormatValueNotANumber()
+    {
+        self::expectException(InvalidValueException::class);
+        self::expectExceptionCode(10);
+
+        $messageFormatter = new PrecisionNumberFormatter('de_DE');
+
+        $messageFormatter->formatValue('.2number', 'notANumber');
+    }
+
+    /**
+     * @dataProvider provideNumber
+     *
+     * @param string $typeSpecifier
+     * @param float $number
+     * @param string $expects
+     */
+    public function testFormatValueNumber(string $typeSpecifier, float $number, string $expects)
+    {
+        $messageFormatter = new PrecisionNumberFormatter('de_DE');
+
+        self::assertSame($expects, $messageFormatter->formatValue($typeSpecifier, $number));
+    }
+
+    /**
+     * @return array
+     */
+    public function provideNumber() : array
+    {
+        return [
+            'comma' => ['.2number', 1.2, '1,20'],
+            'thousand-separator' => ['.3number', 1001.2, '1.001,200'],
+            'million-separator' => ['.3number', 1003001.2, '1.003.001,200'],
+            'zero-comma' => ['.1number', 0.20001, '0,2'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function provideTypeSpecifier() : array
+    {
+        return [
+            ['.2number'],
+            ['.0number'],
+            ['.100number'],
+        ];
+    }
+}
