52804c8475d6e35a74c8abc65c7c931674c3403a
Claudio Zizza <claudio@budgegeria.de>
2020-09-16 22:52:43 +0200

    Add integration test for padding in type specifier

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -30,6 +30,7 @@ class FactoryTest extends TestCase
         self::assertSame('Today is 3/1/16', $intlFormat->format('Today is %date_short', $date));
         self::assertSame('I got 1,002.25 as average value', $intlFormat->format('I got %number as average value', 1002.25));
         self::assertSame('I got 1,002.2500 as average value', $intlFormat->format('I got %.4number as average value', 1002.25));
+        self::assertSame('I got 01,002.2500 as average value', $intlFormat->format('I got %011.4number as average value', 1002.25));
         self::assertSame('I is 5:30 AM on my clock.', $intlFormat->format('I is %time_short on my clock.', $date));
         self::assertSame('The timezone id is US/Arizona.', $intlFormat->format('The timezone id is %timeseries_id.', $date));
         self::assertSame('I am from Italy.', $intlFormat->format('I am from %region.', 'it_IT'));
