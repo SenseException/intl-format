48645492528b9826d38e62117cf802d3052af525
Claudio Zizza <claudio@budgegeria.de>
2022-11-02 22:56:15 +0100

    Replace magic numbers with constants in exception classes

--- src/Exception/InvalidTypeSpecifierException.php
+++ src/Exception/InvalidTypeSpecifierException.php
@@ -8,19 +8,27 @@ use function sprintf;
 
 class InvalidTypeSpecifierException extends IntlFormatException
 {
+    private const CODE_UNMATCHED_TYPESPECIFIER     = 10;
+    private const CODE_NO_TYPESPECIFIER            = 20;
+    private const CODE_INVALID_TYPESPECIFIER       = 30;
+    private const CODE_INVALID_TYPESPECIFIER_COUNT = 40;
+
     public static function unmatchedTypeSpecifier(string $typeSpecifier): self
     {
-        return new self(sprintf('The type specifier "%s" doesn\'t match with the given values.', $typeSpecifier), 10);
+        return new self(sprintf(
+            'The type specifier "%s" doesn\'t match with the given values.',
+            $typeSpecifier,
+        ), self::CODE_UNMATCHED_TYPESPECIFIER);
     }
 
     public static function noTypeSpecifier(): self
     {
-        return new self('No type specifier are in the message text.', 20);
+        return new self('No type specifier are in the message text.', self::CODE_NO_TYPESPECIFIER);
     }
 
     public static function invalidTypeSpecifier(string $typeSpecifier): self
     {
-        return new self(sprintf('"%s" is not a valid type specifier.', $typeSpecifier), 30);
+        return new self(sprintf('"%s" is not a valid type specifier.', $typeSpecifier), self::CODE_INVALID_TYPESPECIFIER);
     }
 
     public static function invalidTypeSpecifierCount(int $valuesCount, int $typeSpecifiersCount): self
@@ -29,6 +37,6 @@ class InvalidTypeSpecifierException extends IntlFormatException
             'Value count of "%d" doesn\'t match type specifier count of "%d"',
             $valuesCount,
             $typeSpecifiersCount,
-        ), 40);
+        ), self::CODE_INVALID_TYPESPECIFIER_COUNT);
     }
 }
--- src/Exception/InvalidValueException.php
+++ src/Exception/InvalidValueException.php
@@ -10,6 +10,10 @@ use function sprintf;
 
 class InvalidValueException extends IntlFormatException
 {
+    private const CODE_INVALID_VALUETYPE  = 10;
+    private const CODE_INVALID_LOCALE     = 20;
+    private const CODE_INVALID_RETURNTYPE = 30;
+
     /** @param string[] $allowedTypes */
     public static function invalidValueType(mixed $value, array $allowedTypes): self
     {
@@ -17,16 +21,16 @@ class InvalidValueException extends IntlFormatException
             'Invalid type "%s" of value. Allowed types: "%s".',
             gettype($value),
             implode(', ', $allowedTypes),
-        ), 10);
+        ), self::CODE_INVALID_VALUETYPE);
     }
 
     public static function invalidLocale(string $locale): self
     {
-        return new self(sprintf('"%s" is not a valid locale.', $locale), 20);
+        return new self(sprintf('"%s" is not a valid locale.', $locale), self::CODE_INVALID_LOCALE);
     }
 
     public static function invalidReturnType(mixed $value): self
     {
-        return new self(sprintf('Unexpected return type "%s"', gettype($value)), 30);
+        return new self(sprintf('Unexpected return type "%s"', gettype($value)), self::CODE_INVALID_RETURNTYPE);
     }
 }
