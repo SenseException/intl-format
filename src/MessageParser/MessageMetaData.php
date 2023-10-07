08cda4fe448586e8d273809bae569661ed2053a6
Claudio Zizza <claudio@budgegeria.de>
2023-10-07 22:09:55 +0200

    Set MessageMetaData final and properties readonly

--- src/MessageParser/MessageMetaData.php
+++ src/MessageParser/MessageMetaData.php
@@ -12,7 +12,7 @@ namespace Budgegeria\IntlFormat\MessageParser;
  * @psalm-immutable
  * @psalm-internal Budgegeria\IntlFormat
  */
-class MessageMetaData
+final class MessageMetaData
 {
     /**
      * @param array<array-key, string> $parsedMessage
@@ -25,28 +25,22 @@ class MessageMetaData
          *
          * Each element in this array contains either plain text or the type specifier in its pure specified format defined
          * by the MessageParser. Each element has an unique integer key that is referenced by $typeSpecifiers.
-         *
-         * @readonly
          */
-        public array $parsedMessage,
+        public readonly array $parsedMessage,
         /**
          * The list of type specifiers of the parsed message.
          *
          * This array only contains normalized type specifiers that are used in the Formatter classes. The key is an integer
          * value, that has the same value as its position in the $parsedMessage array.
-         *
-         * @readonly
          */
-        public array $typeSpecifiers,
+        public readonly array $typeSpecifiers,
         /**
          * The list of values.
          *
          * This list contains the values, that need to be formatted by the Formatter classes. The values are already swapped
          * and in the same order as the $typeSpecifiers representing it.
-         *
-         * @readonly
          */
-        public array $values,
+        public readonly array $values,
     ) {
     }
 }
